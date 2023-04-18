6af10dd5cb89cd23f477a41aa786ec701ad50f97
viphanf <120346488+viphanf@users.noreply.github.com>
2023-04-18 19:44:50 +0800

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ const {CookieAccessInfo, CookieJar, Cookie} = cookiejar
 
 let config = {
     httpprefix: 'https', port: 443,
-    serverName: 'siteproxy-pearl-eight.vercel.app',
+    serverName: 'site001.vercel.app',
 }
 let blockedSites = ['www.chase.com'] // accessing to chase.com was reported by google
 
